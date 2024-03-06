3f2dfce8756472cfcef5cbef70c6ceaa6fb1814f
Haritha <potnooru.haritha@capgemini.com>
2024-03-06 14:38:47 +0530

    First

---
+++ src/com/cg/Demo/FirstGit.java
@@ -0,0 +1,8 @@
+package com.cg.Demo;
+
+public class FirstGit {
+	public static void main(String ar[]) {
+		System.out.println("hi gittu");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author pharitha
+ *
+ */
+module GitDemo {
+}
